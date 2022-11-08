ed845b93126c880e2a40a59c0bfa493e41c6201e
Ron <ron00003@gmail.com>
2022-11-09 01:09:13 +0200

    changing navbar and fixing mobile nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
-import { Navbar } from "./components/Navbar";
 import { Projects } from "./components/Projects";
 import { ScrollTopButton } from "./components/ScrollTopButton";
 import { Skills } from "./components/Skills";
+import { Navbar } from './components/Navbar';
 
 function App() {
   return (
