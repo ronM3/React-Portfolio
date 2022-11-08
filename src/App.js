5188dede7aa0a4595b7b27b3f822475f3c485dc1
Ron <ron00003@gmail.com>
2022-11-09 00:12:05 +0200

    changing navbar and fixing mobile nav

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import './App.css';
-import { Footer } from './components/Footer';
-import { Header } from './components/Header';
-import { Navbar } from './components/Navbar';
-import { Projects } from './components/Projects';
-import { ScrollTopButton } from './components/ScrollTopButton';
-import { Skills } from './components/Skills';
+import "./App.css";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import { Navbar } from "./components/Navbar";
+import { Projects } from "./components/Projects";
+import { ScrollTopButton } from "./components/ScrollTopButton";
+import { Skills } from "./components/Skills";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-        <ScrollTopButton/>
-        <Header/>
-        <Skills/>    
-        <Projects/>
-        <Footer/>
+      <Navbar />
+      <ScrollTopButton />
+      <Header />
+      <Skills />
+      <Projects />
+      <Footer />
     </div>
   );
 }
